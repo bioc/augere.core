555f0a5f688e21e3162a5ca39f5d0c5e8c456db9
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-01-08 18:23:03 +1100

    Minor improvements to pass Bioc review, mostly to flesh out details.

---
+++ R/augere.core-package.R
@@ -0,0 +1,8 @@
+#' augere.core
+#'
+#' Implement utilities for the augere framework,
+#' to automatically generate reports for common bioinformatics analyses.
+#' See downstream packages like \pkg{augere.de} and \pkg{augere.screen} for available analysis pipelines.
+#'
+#' @name augere.core-package
+NULL
